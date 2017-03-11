ab6b12bf13287ecab928dee9f1ccad5de700cac2
Kelly Hong <kellyhong@telmate.com>
2017-03-10 23:03:57 -0800

    simplify battle logic

--- lib/battle_logic.rb
+++ lib/battle_logic.rb
@@ -1,109 +1,91 @@
-# manage resolving spell execution, monster defeats check, health decrease, resolving turns and wins/losses etc.
+# Responsibility:: manage resolving spell execution & monster defeats check
+# Collaborators: monster.rb, defeat_condition.rb
+require './lib/defeat_conditions'
+
 class BattleLogic
 
-  # control how battles are fought
-  def battle_flow(monster_type, player, monster_counter, monster_limit)
-    while monster_counter <= monster_limit
-      require './lib/monster'
+  # get defeat_condition for monster
+  def fetch_defeat_condition
 
-      monster = Monster.new
-      enemy = monster.spawn_monster(monster_type)
-      monster.announce_monster(enemy)
-
-      show_monster_defeat_condition
-
-      @player.display_health
-      display_available_spells
-      inputted_spell = activated_spell
-      result = execute_spell(enemy, inputted_spell)
-      check_defeat_result(result, expected_result(enemy))
-    end
-  end
-
-  private
-
-  # return what monster looks like when it is defeated
-  def expected_result(monster)
-    monster.downcase
-  end
-
-  def successfully_defeated_monster
-    player.health += 1
-    scoreboard.update('player1', 1, +1)
-  end
-
-  # resolve if spell defeated monster
-  def check_defeat_result(result, expected_result)
-    unless result == expected_result
-      puts "Your spell failed."
-      lose_one_health
-    end
   end
 
   # display condition needed to defeat monster
-  def show_monster_defeat_condition
+  def show_defeat_condition(defeat_condition)
     puts '-' * 40
     puts
-    print "DEFEAT CONDITION:"
-    print "To defeat this monster you must..."
-    print "make it smaller (turn in into lowercase form.)"
+    puts "DEFEAT CONDITION:"
+    puts "To defeat this monster you must...#{defeat_condition}"
     puts
-    #print "turn it into an ARRAY type monster"
   end
 
-  # show unlocked spells
-  def display_available_spells
-    require './lib/spell_book'
-    book = SpellBook.new
-    puts '-' * 40
-    puts
-    puts "YOUR SPELLS: #{book.spells}"
-    puts
-    puts '-' * 40
+  def initiate_battles(monster_type, player, monster_counter, monster_limit)
+    require './lib/battle_logic'
+
+    battle = BattleLogic.new
+    battle.battle_flow(monster_type, player, monster_counter, monster_limit)
   end
+  # # control how battles are fought
+  # def battle_flow(monster_type, player, monster_counter, monster_limit)
+  #   while monster_counter <= monster_limit
+  #     require './lib/monster'
+  #
+  #     monster = Monster.new
+  #     enemy = monster.spawn_monster(monster_type)
+  #     monster.announce_monster(enemy)
+  #
+  #     show_monster_defeat_condition
+  #
+  #     @player.display_health
+  #     display_available_spells
+  #     inputted_spell = activated_spell
+  #     result = execute_spell(enemy, inputted_spell)
+  #     check_defeat_result(result, expected_result(enemy))
+  #   end
+  # end
 
-  # return spell user inputs
-  def activated_spell
-    puts "Please type the spell you want to cast:"
-    gets.chomp.strip
-  end
+  private
 
-  # cast spell on monster / invoke method on object
-  def execute_spell(monster, spell)
-    puts "You cast the #{spell} spell:"
-    print "which turns the #{monster} into..."
-    print eval ("'#{monster}'.#{spell}")
-  end
-end
+  # # return what monster looks like when it is defeated
+  # def expected_result(monster)
+  #   monster.downcase
+  # end
+  #
+  # def successfully_defeated_monster
+  #   player.health += 1
+  #   scoreboard.update('player1', 1, +1)
+  # end
+
+  # # resolve if spell defeated monster
+  # def check_defeat_result(result, expected_result)
+  #   unless result == expected_result
+  #     puts "Your spell failed."
+  #     lose_one_health
+  #   end
+  # end
 
 
 
-b = BattleLogic.new
-b.run(stage1, win_count)
+
+#   # cast spell on monster / invoke method on object
+#   def execute_spell(monster, spell)
+#     puts "You cast the #{spell} spell:"
+#     print "which turns the #{monster} into..."
+#     print eval ("'#{monster}'.#{spell}")
+#   end
+# end
 
 
 
 
-# TODO move this to battle logic
-def initiate_battles(monster_type, player, monster_counter, monster_limit)
-  require './lib/battle_logic'
 
-  battle = BattleLogic.new
-  battle.battle_flow(monster_type, player, monster_counter, monster_limit)
-end
 
-# display banner when entering stage
-def display_stage_banner(number, name, description)
-  puts '*' * 30
-  puts "Entering STAGE #{number} - #{name}..."
-  puts description
-  puts '*' * 30
-end
 
-# check score board to see if player passes a stage
-def completed?(stage_id, player)
-  scoreboard.display(player)
-  if [stage_id] == 10
-    puts "you passed stage #{stage_id}"
+
+  # check score board to see if player passes a stage
+  def completed?(stage_id, player)
+    scoreboard.display(player)
+    if [stage_id] == 10
+      puts "you passed stage #{stage_id}"
+    end
   end
 end
